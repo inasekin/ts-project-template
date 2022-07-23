b63bee4cc064004d180598f74ba2f33726846d98
Ivan Nasekin <nasekin@ipavlov.ai>
2022-07-23 23:25:09 +0300

    добавил основные настройки

---
+++ src/__test__/main.test.ts
@@ -0,0 +1,31 @@
+import { expect } from 'chai';
+import { JSDOM } from 'jsdom';
+import * as utils from '../utils';
+import sinon, { SinonStub } from 'sinon';
+
+const createJsDocument = () => {
+  const dom = new JSDOM(
+    `<html>
+         <body>           
+         </body>
+       </html>`,
+    { url: 'http://localhost' }
+  );
+  return dom.window.document;
+};
+
+describe('main program', () => {
+  let createHeaderSpy: SinonStub<string[], HTMLHeadingElement>;
+  beforeEach(() => {
+    global.document = createJsDocument();
+    createHeaderSpy = sinon.stub(utils, 'createHeader');
+  });
+
+  it('should render result of createHeader', async () => {
+    const d1 = document.createElement('h1');
+    createHeaderSpy.returns(d1);
+    await import('../main.js');
+
+    expect(global.document.body.querySelector('h1') === d1).to.eq(true);
+  });
+});
